d26223e2e546d2e48c21f17a89d6f49dac816695
Parbat Upadhaya <parbatupadhaya2@gmail.com>
2026-09-14 23:29:31 +0545

    Update views

--- ecommerce/blog/views.py
+++ ecommerce/blog/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello blog  world")
+    return render(request, "blog/index.html")
