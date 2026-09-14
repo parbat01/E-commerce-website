7a6199ad64318cfef4f3bfa31b946c2ae0e413f8
Parbat Upadhaya <parbatupadhaya2@gmail.com>
2026-09-14 23:34:52 +0545

    Update urls and views in shop app

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -4,4 +4,10 @@ from shop import views
 
 urlpatterns = [
     path("", views.index, name="shophome"),
+    path("about/", views.about, name="aboutus"),
+    path("contact/", views.contact, name="contact"),
+    path("tracker/", views.tracker, name="tracker"),
+    path("search/", views.search, name="search"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("productview/", views.productview, name="productview"),
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello shop  world")
+    return render(request, "shop/index.html")
+
+
+def about(request):
+    return render(request, "shop/about.html")
+
+
+def contact(request):
+    return render(request, "shop/contact.html")
+
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+
+def search(request):
+    return HttpResponse("we are at search")
+
+
+def checkout(request):
+    return HttpResponse("we are at checkout")
+
+
+def productview(request):
+    return HttpResponse("we are at productview")
