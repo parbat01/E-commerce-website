aaa299c61aa6ddc06d66daae594395775c6e23a3
Parbat Upadhaya <parbatupadhaya2@gmail.com>
2026-09-14 23:30:19 +0545

    Added models to the models.py files

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    product_no = models.AutoField
+    product_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    publish_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
